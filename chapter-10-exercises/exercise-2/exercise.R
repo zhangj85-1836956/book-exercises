6655e96f0e0dad6ff7a7160ce61c09cf034fef1d
Jin Zhang <zhangj85@uw.edu>
2020-01-25 01:12:01 -0800

    Upload week 3 work

--- chapter-10-exercises/exercise-2/exercise.R
+++ chapter-10-exercises/exercise-2/exercise.R
@@ -16,15 +16,15 @@ adjustments <- runif(100, -5000, 10000)
 
 # Create a data frame `salaries` by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salaries_2017, adjustments, satringAsFactors=FALSE)
 
 # Add a column to the `salaries` data frame that represents each person's
 # salary in 2018 (e.g., with the salary adjustment added in).
-
+salaries$salaries_2018 <- salaries$salaries_2017 + salaries$adjustments
 
 # Add a column to the `salaries` data frame that has a value of `TRUE` if the 
 # person got a raise (their salary went up)
-
+salaries$got_raise <- salaries$adjustments > 0
 
 
 ### Retrieve values from your data frame to answer the following questions
@@ -32,13 +32,14 @@ adjustments <- runif(100, -5000, 10000)
 ### cell rather than the whole row!)
 
 # What was the 2018 salary of Employee 57
-
+print(salaries[salaries$employees == "Employee 57", "salaries_2018"])
 
 # How many employees got a raise?
-
+nrow(salaries[salaries$got_raise == TRUE, ])
 
 # What was the dollar value of the highest raise?
-
+highest_raise <- max(salaries$adjustments)
+salaries[ salaries$adjustments == max(salaries$adjustments), "adjustments"]
 
 # What was the "name" of the employee who received the highest raise?
 
@@ -53,7 +54,8 @@ adjustments <- runif(100, -5000, 10000)
 
 
 # For people who did not get a raise, how much money did they lose on average?
-
+paycut <- salaries[salaries$got_raise == FALSE, ]
+mean(paycut$adjustments)
 
 ## Consider: do the above averages match what you expected them to be based on 
 ## how you generated the salaries?
